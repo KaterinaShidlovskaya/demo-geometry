731762d9eac255619bd5ea2cce8174daa72714ac
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-05-04 01:15:53 +0300

    refactor boxdemo class add one more box object

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -3,14 +3,20 @@ class Box {
     double height;
     double depth;
 }
-class BoxDemo {
+class BoxDemo2 {
     public static void main(String[] args) {
-        Box mybox = new Box();
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
         double vol;
-        mybox.width = 10;
-        mybox.height = 20;
-        mybox.depth = 15;
-        vol = mybox.width * mybox.height * mybox.depth;
+        mybox1.width = 10;
+        mybox1.height = 20;
+        mybox1.depth = 15;
+        mybox2.width = 3;
+        mybox2.height = 6;
+        mybox2.depth = 9;
+        vol = mybox1.width * mybox1.height * mybox1.depth;
+        System.out.println("Объем равен " + vol);
+        vol = mybox2.width * mybox2.height * mybox2.depth;
         System.out.println("Объем равен " + vol);
     }
 }
