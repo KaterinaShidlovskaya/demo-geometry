68a222f4f8eadd29c8fb81656077c3e193850a21
Katerina Shidlovskaya <katerinashidlovskaya2711@gmail.com>
2023-05-11 02:00:17 +0300

    refactor box class add constructor

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -2,24 +2,23 @@ class Box {
     double width;
     double height;
     double depth;
+    Box(){
+        System.out.println("Конструирование объекта Box");
+        width=10;
+        height=10;
+        depth=10;
+    }
 
     double volume() {
         return width * height * depth;
     }
-    void setDim(double w, double h, double d) {
-        width = w;
-        height =h;
-        depth=d;
-    }
 }
 
-class BoxDemo5 {
+class BoxDemo6 {
     public static void main(String[] args) {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
         double vol;
-        mybox1.setDim(10,20,15);
-        mybox2.setDim(3,6,9);
         vol=mybox1.volume();
         System.out.println("Объем равен " + vol);
         vol=mybox2.volume();
